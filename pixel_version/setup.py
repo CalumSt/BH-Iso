f74f3c065d8eebde96ed40fa1adb30901e17895a
CalumSt <92938077+CalumSt@users.noreply.github.com>
2021-12-13 15:50:23 +0000

    Add files via upload

--- pixel_version/setup.py
+++ pixel_version/setup.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from detections import Event, Run
 
-GenPath = '/root/Home/BH-Iso/'
+GenPath = '/home/2311453s/BH-Iso/'
 
 def events_list(nposts=5000, simulated=False, multiplicity=10):
     events = list()
@@ -16,26 +16,26 @@ def events_list(nposts=5000, simulated=False, multiplicity=10):
     for name in O1_events:
         print('Loading event {}'.format(name))
         run = 'O1'
-        samp = '../../Data/GWTC-1_sample_release/{}_GWTC-1.hdf5'.format(name)
-        pofd = '../../Data/Run_O1/Events/Pofd_{}_marg.p'.format(name)
-        time = int(np.genfromtxt('../../Data/times/{}.txt'.format(name)))
+        samp = GenPath + 'Data/GWTC-1_sample_release/{}_GWTC-1.hdf5'.format(name)
+        pofd = GenPath + 'Data/Run_O1/Events/Pofd_{}_marg.p'.format(name)
+        time = int(np.genfromtxt(GenPath + 'Data/times/{}.txt'.format(name)))
         events.append(Event(name, run, time, samp, pofd, nposts=nposts))
 
     for name in O2_events:
         print('Loading event {}'.format(name))
         run = 'O2'
-        samp = '../../Data/GWTC-1_sample_release/{}_GWTC-1.hdf5'.format(name)
-        pofd = '../../Data/Run_O2/Events/Pofd_{}_marg.p'.format(name)
-        time = int(np.genfromtxt('../../Data/times/{}.txt'.format(name)))
+        samp = GenPath + 'Data/GWTC-1_sample_release/{}_GWTC-1.hdf5'.format(name)
+        pofd = GenPath + 'Data/Run_O2/Events/Pofd_{}_marg.p'.format(name)
+        time = int(np.genfromtxt(GenPath + 'Data/times/{}.txt'.format(name)))
         events.append(Event(name, run, time, samp, pofd, nposts=nposts))
 
     return events
 
 def runs_list():
-    mean_pofd_path_O1 = '../../Data/Run_O1/Events/Pofd_early_average.p'
-    obs_times_O1 = '../../Data/PSD_data/Obs_time_O1.txt'
-    mean_pofd_path_O2 = '../../Data/Run_O2/Events/Pofd_mid_average.p'
-    obs_times_O2 = '../../Data/PSD_data/Obs_time_O2.txt'
+    mean_pofd_path_O1 = GenPath + 'Data/Run_O1/Events/Pofd_early_average.p'
+    obs_times_O1 = GenPath + 'Data/PSD_data/Obs_time_O1.txt'
+    mean_pofd_path_O2 = GenPath + 'Data/Run_O2/Events/Pofd_mid_average.p'
+    obs_times_O2 = GenPath + 'Data/PSD_data/Obs_time_O2.txt'
     runs = [Run('O1', mean_pofd_path_O1, obs_times_O1),
             Run('O2', mean_pofd_path_O2, obs_times_O2)]
     print('Loaded runs')
